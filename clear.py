6522403043aeabfc120a54b738e27a2f706ab63f
Efloud <68462068+Efloud@users.noreply.github.com>
2022-03-18 21:47:04 +0300

    Add files via upload

---
+++ clear.py
@@ -0,0 +1,18 @@
+####
+
+# # #
+# File Clearing Start and End characters
+# # #
+
+####
+
+print('\n')
+location = input('Please File Location => ')
+file = open(location)
+read = file.read()
+print('\n')
+clear = input('Which character do you want to clear? => ')
+
+for i in read.split():
+    print(i.strip(clear), end='\n')
+file.close()
