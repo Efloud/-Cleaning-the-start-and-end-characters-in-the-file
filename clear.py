66c86bef31a7ce83d83aa734eb7125a531c3ecd4
Efloud <68462068+Efloud@users.noreply.github.com>
2022-03-18 22:24:03 +0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -8,7 +8,9 @@ try:
     read = file.read()
     print('\n')
     clear = input('Which character do you want to clear? => ')
+    
     for i in read.split():
         print(i.strip(clear), end='\n')
+        
 except FileNotFoundError:
-    print(' File Path İs Wrong !')
+    print('File Path İs Wrong !')
