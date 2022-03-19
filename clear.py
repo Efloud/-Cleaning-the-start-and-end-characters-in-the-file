35291878f4c46642cfb4623780ac681b1d3b143c
Efloud <68462068+Efloud@users.noreply.github.com>
2022-03-20 01:42:40 +0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -2,16 +2,21 @@
 # 𝕰𝖋𝖑𝖔𝖚𝖉
 
 try:
+
     print('\n')
     location = input('Please File Location => ')
     file = open(location)
     read = file.read()
     print('\n')
-    clear = input('Which character do you want to clear? => ')
-    
+    clear = input('[+] Which character do you want to clear? => ')
+    print('\n')
+    save = input('[+] Specify where to save the file :')
+    output = open(save, 'w', encoding='utf-8')
+
     for i in read.split():
-        print(i.strip(clear), end='\n')
-        
+        print(i.strip(clear), end='\n', file=output)
+
+
 except FileNotFoundError:
     print('File Path İs Wrong !')
-file.close()
+
