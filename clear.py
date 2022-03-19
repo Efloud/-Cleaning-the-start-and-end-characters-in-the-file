dad174c8245b4ba193e684c1ae89cc22d4a8a198
Efloud <68462068+Efloud@users.noreply.github.com>
2022-03-20 02:33:10 +0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -16,7 +16,7 @@ try:
     for i in read.split():
         print(i.strip(clear), end='\n', file=output)
 
-    print('success')
+    print('\n', '[+] File {} location saved. '.format(save), sep='')
 except FileNotFoundError:
     print('File Path İs Wrong !')
 
