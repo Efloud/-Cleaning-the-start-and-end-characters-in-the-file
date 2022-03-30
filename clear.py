33cffdb554d82e1ad4ccb17b17b44317c1a95e16
Efloud <68462068+Efloud@users.noreply.github.com>
2022-03-31 00:26:07 +0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,5 +1,6 @@
 #python3
-print("""                                                                                           
+print(
+ """                                                                                           
 @@@@@@@   @@@       @@@@@@@@  @@@@@@    @@@@@@@   
 @@@@@@@@  @@@       @@@@@@@@  @@@@@@@@  @@@@@@@@  
 !@@       @@!       @@!       @@!  @@@  @@!  @@@  
@@ -10,29 +11,30 @@ print("""
 :!:        :!:      :!:       :!:  !:!  :!:  !:!  
  ::: :::   :: ::::   :: ::::  ::   :::  ::   :::  
  :: :: :  : :: : :  : :: ::    :   : :   :   : :  
-""", '\n')
+""", 
+      '\n')
 try:
 
-    location = input('[x] Please File Location => ')
+    location = input("[x] Please File Location => ")
     
     if not location.endswith('txt'):
-        print('please give the extension as txt')
+        print("please give the extension as txt")
         
     for i in location:
-        rep = i.replace('/', "\\")
+        rep = i.replace("/", "\\")
         
     file = open(location)
     read = file.read()
-    print('\n')
-    clear = input('[+] Which character do you want to clear? => ')
-    print('\n')
-    save = input('[+] Specify where to save the file : ')
-    output = open(save, 'w', encoding='utf-8')
+    print("\n")
+    clear = input("[+] Which character do you want to clear? => ")
+    print("\n")
+    save = input("[+] Specify where to save the file : ")
+    output = open(save, "w", encoding="utf-8")
 
     for i in read.split():
-        print(i.strip(clear), end='\n', file=output)
+        print(i.strip(clear), end="\n", file=output)
 
-    print('\n', f'[+] File: {save} location saved', sep='')
+    print("\n", f"[+] File: {save} location saved", sep="")
     
 except FileNotFoundError:
-    print('File Path İs Wrong !')
+    print("File Path İs Wrong !")
