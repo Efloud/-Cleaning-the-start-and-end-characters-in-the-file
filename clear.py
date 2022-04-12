af72d7401c76bcc9a66f17f579fa9d0da3bbc17c
Efloud <68462068+Efloud@users.noreply.github.com>
2022-04-12 03:11:04 +0300

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,5 +1,4 @@
 import time
-
 print(
  """                                                                                           
 ▓█████   █████▒▄████▄   ██▓    ▓█████ ▄▄▄       ██▀███  
@@ -14,12 +13,11 @@ print(
               ░                                         
 """, '\n')
 
-time.sleep(1.0)
 location = input("[x] Temizlemek istediğiniz Dosya konumunu Girin => ")
 
 if not location.endswith("txt"):
-    print("lütfen uzantıyı txt olarak verin.")
-
+    print("txt girin !")
+    quit()
 for i in location:
     rep = i.replace("/", "\\")
 
@@ -28,17 +26,23 @@ try:
     with open(location, "r") as file:
         read = file.read()
         print("\n")
-        time.sleep(1.0)
+        time.sleep(0.5)
         clear = input("[+] Temizlemek İstediğiniz Karakterler ? => ")
         print("\n")
-        time.sleep(1.0)
+        time.sleep(0.5)
         save = input("[+] Dosyanın Kaydedileceği Konumu Girin : ")
+
+        if not save.endswith("txt"):
+            print("txt girin !")
+            quit()
         output = open(save, 'w', encoding='utf-8')
+
 
     for i in read.split():
         print(i.strip(clear), end="\n", file=output)
 
+    time.sleep(0.5)
     print("\n", f"[+] Dosya : {save} konumuna kaydedildi.", sep="")
 
 except FileNotFoundError:
-    print("Belirtmiş olduğunuz Dosya Konumu Bulunamadı !")
+    print("Dosya Konumu Bulunamadı !")
